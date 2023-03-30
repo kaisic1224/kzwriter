5f98246d47b2c133ff482cd97aaa59f148aaae8e
kaisic1224 <89894485+kaisic1224@users.noreply.github.com>
2023-03-29 22:53:15 -0400

    Add files via upload

---
+++ lua/plugins/config/cmp.lua
@@ -0,0 +1,86 @@
+ local cmp = require'cmp'
+vim.opt.completeopt = "menu,menuone,noinsert"
+
+  cmp.setup({
+    snippet = {
+      expand = function(args)
+         require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      end,
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-f>'] = cmp.mapping.scroll_docs(4),
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-e>'] = cmp.mapping.abort(),
+      ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+    }),
+    sources = cmp.config.sources({
+      { name = 'nvim_lsp' },
+      { name = "nvim_lua" },
+      { name = "luasnip" },
+      { name = "path" },
+    }, {
+      { name = 'buffer', keyword_length = 3 },
+    })
+  })
+
+  -- Use buffer source for `/` and `?` (if you enabled `native_menu`, this won't work anymore).
+  cmp.setup.cmdline({ '/', '?' }, {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = {
+      { name = 'buffer' }
+    }
+  })
+
+  cmp.setup.cmdline(':', {
+    mapping = cmp.mapping.preset.cmdline(),
+    sources = cmp.config.sources({
+      { name = 'path' }
+    }, {
+      { name = 'cmdline' }
+    })
+  })
+
+
+  -- Setup buffer-local keymaps / options for LSP buffers
+  local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
+  local lsp_attach = function(client, buf)
+	-- Example maps, set your own with vim.api.nvim_buf_set_keymap(buf, "n", <lhs>, <rhs>, { desc = <desc> })
+	-- or a plugin like which-key.nvim
+	-- <lhs>        <rhs>                        <desc>
+	-- "K"          vim.lsp.buf.hover            "Hover Info"
+	-- "<leader>qf" vim.diagnostic.setqflist     "Quickfix Diagnostics"
+	-- "[d"         vim.diagnostic.goto_prev     "Previous Diagnostic"
+	-- "]d"         vim.diagnostic.goto_next     "Next Diagnostic"
+	-- "<leader>e"  vim.diagnostic.open_float    "Explain Diagnostic"
+	-- "<leader>ca" vim.lsp.buf.code_action      "Code Action"
+	-- "<leader>cr" vim.lsp.buf.rename           "Rename Symbol"
+	-- "<leader>fs" vim.lsp.buf.document_symbol  "Document Symbols"
+	-- "<leader>fS" vim.lsp.buf.workspace_symbol "Workspace Symbols"
+	-- "<leader>gq" vim.lsp.buf.formatting_sync  "Format File"
+
+	vim.api.nvim_buf_set_option(buf, "formatexpr", "v:lua.vim.lsp.formatexpr()")
+	vim.api.nvim_buf_set_option(buf, "omnifunc", "v:lua.vim.lsp.omnifunc")
+	vim.api.nvim_buf_set_option(buf, "tagfunc", "v:lua.vim.lsp.tagfunc")
+  end
+
+  -- Setup rust_analyzer via rust-tools.nvim
+ require("rust-tools").setup({
+tools = {
+    inlay_hints = {
+      auto = true,
+      show_parameter_hints = false,
+      parameter_hints_prefix = "",
+      other_hints_prefix = "",
+}
+    },
+	server = {
+		capabilities = capabilities,
+		on_attach = lsp_attach,
+		["rust-analyzer"] = {
+			checkOnSave = {
+				command = "clippy"
+			}	
+		}
+	}
+  })
---
+++ lua/plugins/init.lua
@@ -0,0 +1,208 @@
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then 
+        print "Waittt, bootstrapping lazy.nvim ..."
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+local ok, lazy = pcall(require, 'lazy')
+if not ok then return end
+lazy.setup({
+
+        -- Colorscheme --
+--{
+--        'rose-pine/neovim',
+--        name = 'rose-pine',
+--        lazy = false,
+--        priority = 1000,
+--        config = function()
+--                vim.cmd('colorscheme rose-pine')
+--        end
+--},
+--
+{
+        'catppuccin/nvim',
+        name = 'catppuccin',
+        lazy = false,
+        priority = 1000,
+        config = function()
+                vim.cmd('colorscheme catppuccin-mocha')
+        end
+},
+
+
+--{
+--        'Everblush/nvim',
+--        name = 'Everblush',
+--        lazy = false,
+--        priority = 1000,
+--        config = function()
+--                vim.cmd('colorscheme everblush')
+--        end
+--},
+        -- Autopairings --
+	    { 
+    		'windwp/nvim-autopairs',
+    		name = 'nvim-autopairs',
+    		lazy = false,
+    		config = function()
+      			require("nvim-autopairs").setup()
+    		end
+	    },
+		        -- lsp stuffs --
+	    	{
+        		'hrsh7th/cmp-nvim-lsp',
+			'hrsh7th/cmp-nvim-lua',
+        		'hrsh7th/cmp-buffer',
+        		'hrsh7th/cmp-path',
+        		'hrsh7th/cmp-cmdline',
+			'saadparwaiz1/cmp_luasnip',
+			'simrat39/rust-tools.nvim',
+			'L3MON4D3/LuaSnip',
+			'neovim/nvim-lspconfig',
+	    	},
+
+{
+        
+        'hrsh7th/nvim-cmp',
+            config = function()
+                    require("plugins.config.cmp")
+            end
+
+
+},
+
+        -- Bufferline --
+
+{
+
+    'akinsho/bufferline.nvim',
+    name = 'bufferline',
+    lazy = false,
+    priority = 1000,
+    config = function()
+        require("plugins.config.bufferline")
+    end
+},
+        -- icon pack for bufferline --
+{
+        'kyazdani42/nvim-web-devicons',
+	lazy = true
+},
+
+        -- Nvim tree file viewer --
+
+{
+        'nvim-tree/nvim-tree.lua',
+        name = 'nvim-tree',
+        lazy = false,
+        config = function()
+            require("plugins.config.nvim-tree")
+        end
+},
+
+        -- Telescope stuffs -- 
+
+        {
+                'nvim-lua/plenary.nvim',
+		lazy = true
+        },
+
+{
+        
+        'alvarosevilla95/telescope.nvim',
+        name = 'telescope',
+        lazy = true,
+        priotity = 1000,
+        config = function()
+                require("plugins.config.telescope")
+        end
+},
+
+        -- Treesitter stuffs --
+{
+        'nvim-treesitter/nvim-treesitter',
+        name = 'treesitter',
+        lazy = true,
+        config = function()
+                require("plugins.config.treesitter")
+        end
+},
+
+        -- Statusline (staline) --
+
+{
+        'tamton-aquib/staline.nvim',
+        name = 'staline',
+        lazy = false,
+        config = function()
+                require("plugins.config.staline")
+        end
+},
+
+        -- Floating terminal --
+{
+        'akinsho/toggleterm.nvim',
+        name = 'toggleterm',
+        lazy = true,
+        config = function()
+                require("plugins.config.term")
+        end
+},
+
+        -- gitsigns stuffs --
+
+{
+        'lewis6991/gitsigns.nvim',
+        name = 'gitsigns',
+        lazy = true,
+        config = function()
+                require("plugins.config.gitsigns")
+        end
+},
+
+
+
+},{ 
+performance = {
+    rtp = {
+      disabled_plugins = {      
+        "2html_plugin",
+        "tohtml",
+        "getscript",
+        "getscriptPlugin",
+        "gzip",
+        "logipat",
+        "netrw",
+        "netrwPlugin",
+        "netrwSettings",
+        "netrwFileHandlers",
+        "matchit",
+        "tar",
+        "tarPlugin",
+        "rrhelper",
+        "spellfile_plugin",
+        "vimball",
+        "vimballPlugin",
+        "zip",
+        "zipPlugin",
+        "tutor",
+        "rplugin",
+        "syntax",
+        "synmenu",
+        "optwin",
+        "compiler",
+        "bugreport",
+        "ftplugin",
+},
+},
+},                                                                                                                                                                                                                          
+}
+)                                                                                                                                                                                                                            
