084dee6f326a4a1807cca89b41c2ad3ab70aa777
Kaisic1224 <kaisic1224@gmail.com>
2023-04-14 21:14:45 -0400

    updated

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -38,85 +38,76 @@ lazy.setup({
 },
 
 
---{
---        'Everblush/nvim',
---        name = 'Everblush',
---        lazy = false,
---        priority = 1000,
---        config = function()
---                vim.cmd('colorscheme everblush')
---        end
---},
-        -- Autopairings --
-	    { 
-    		'windwp/nvim-autopairs',
-    		name = 'nvim-autopairs',
-    		lazy = false,
-    		config = function()
-      			require("nvim-autopairs").setup()
-    		end
-	    },
-		        -- lsp stuffs --
-	    	{
-        		'hrsh7th/cmp-nvim-lsp',
-			'hrsh7th/cmp-nvim-lua',
-        		'hrsh7th/cmp-buffer',
-        		'hrsh7th/cmp-path',
-        		'hrsh7th/cmp-cmdline',
-			'saadparwaiz1/cmp_luasnip',
-			'simrat39/rust-tools.nvim',
-			'L3MON4D3/LuaSnip',
-			'neovim/nvim-lspconfig',
-	    	},
+-- Autopairings --
+{ 
+        'windwp/nvim-autopairs',
+        name = 'nvim-autopairs',
+        lazy = false,
+        config = function()
+                require("nvim-autopairs").setup()
+        end
+},
+-- lsp stuffs --
+{
+        'hrsh7th/cmp-nvim-lsp',
+        'hrsh7th/cmp-nvim-lua',
+        'hrsh7th/cmp-buffer',
+        'hrsh7th/cmp-path',
+        'hrsh7th/cmp-cmdline',
+        'saadparwaiz1/cmp_luasnip',
+        'simrat39/rust-tools.nvim',
+        'L3MON4D3/LuaSnip',
+        'neovim/nvim-lspconfig',
+},
 
 {
-        
+
         'hrsh7th/nvim-cmp',
-            config = function()
-                    require("plugins.config.cmp")
-            end
+        config = function()
+                require("plugins.config.cmp")
+        end
 
 
 },
 
-        -- Bufferline --
+-- Bufferline --
 
 {
 
-    'akinsho/bufferline.nvim',
-    name = 'bufferline',
-    lazy = false,
-    priority = 1000,
-    config = function()
-        require("plugins.config.bufferline")
-    end
+        'akinsho/bufferline.nvim',
+        name = 'bufferline',
+        lazy = false,
+        priority = 1000,
+        config = function()
+                require("plugins.config.bufferline")
+        end
 },
-        -- icon pack for bufferline --
+-- icon pack for bufferline --
 {
         'kyazdani42/nvim-web-devicons',
-	lazy = true
+        lazy = true
 },
 
-        -- Nvim tree file viewer --
+-- Nvim tree file viewer --
 
 {
         'nvim-tree/nvim-tree.lua',
         name = 'nvim-tree',
         lazy = false,
         config = function()
-            require("plugins.config.nvim-tree")
+                require("plugins.config.nvim-tree")
         end
 },
 
-        -- Telescope stuffs -- 
-
-        {
-                'nvim-lua/plenary.nvim',
-		lazy = true
-        },
+-- Telescope stuffs -- 
 
 {
-        
+        'nvim-lua/plenary.nvim',
+        lazy = true
+},
+
+{
+
         'alvarosevilla95/telescope.nvim',
         name = 'telescope',
         lazy = true,
@@ -126,7 +117,7 @@ lazy.setup({
         end
 },
 
-        -- Treesitter stuffs --
+-- Treesitter stuffs --
 {
         'nvim-treesitter/nvim-treesitter',
         name = 'treesitter',
@@ -136,7 +127,7 @@ lazy.setup({
         end
 },
 
-        -- Statusline (staline) --
+-- Statusline (staline) --
 
 {
         'tamton-aquib/staline.nvim',
@@ -147,7 +138,7 @@ lazy.setup({
         end
 },
 
-        -- Floating terminal --
+-- Floating terminal --
 {
         'akinsho/toggleterm.nvim',
         name = 'toggleterm',
@@ -157,7 +148,7 @@ lazy.setup({
         end
 },
 
-        -- gitsigns stuffs --
+-- gitsigns stuffs --
 
 {
         'lewis6991/gitsigns.nvim',
@@ -171,38 +162,38 @@ lazy.setup({
 
 
 },{ 
-performance = {
-    rtp = {
-      disabled_plugins = {      
-        "2html_plugin",
-        "tohtml",
-        "getscript",
-        "getscriptPlugin",
-        "gzip",
-        "logipat",
-        "netrw",
-        "netrwPlugin",
-        "netrwSettings",
-        "netrwFileHandlers",
-        "matchit",
-        "tar",
-        "tarPlugin",
-        "rrhelper",
-        "spellfile_plugin",
-        "vimball",
-        "vimballPlugin",
-        "zip",
-        "zipPlugin",
-        "tutor",
-        "rplugin",
-        "syntax",
-        "synmenu",
-        "optwin",
-        "compiler",
-        "bugreport",
-        "ftplugin",
-},
-},
-},                                                                                                                                                                                                                          
+        performance = {
+                rtp = {
+                        disabled_plugins = {      
+                                "2html_plugin",
+                                "tohtml",
+                                "getscript",
+                                "getscriptPlugin",
+                                "gzip",
+                                "logipat",
+                                "netrw",
+                                "netrwPlugin",
+                                "netrwSettings",
+                                "netrwFileHandlers",
+                                "matchit",
+                                "tar",
+                                "tarPlugin",
+                                "rrhelper",
+                                "spellfile_plugin",
+                                "vimball",
+                                "vimballPlugin",
+                                "zip",
+                                "zipPlugin",
+                                "tutor",
+                                "rplugin",
+                                "syntax",
+                                "synmenu",
+                                "optwin",
+                                "compiler",
+                                "bugreport",
+                                "ftplugin",
+                        },
+                },
+        },                                                                                                                                                                                                                          
 }
 )                                                                                                                                                                                                                            
