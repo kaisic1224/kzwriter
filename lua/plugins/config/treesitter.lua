cc182fbbfa3d716a568b12f41dd5409a0fab7049
kaisic1224 <hiddenshadows1224@gmail.com>
2023-05-09 18:49:03 -0400

    yes

--- lua/plugins/config/treesitter.lua
+++ lua/plugins/config/treesitter.lua
@@ -1,7 +1,10 @@
 require'nvim-treesitter.configs'.setup {
         -- A list of parser names, or "all" (the four listed parsers should always be installed)
         ensure_installed = {"rust", "typescript", "javascript"},
-
+        autotag = {
+                enable = true,
+                filetypes = { "html", "xml" }
+        }
         -- Install parsers synchronously (only applied to `ensure_installed`)
         sync_install = false,
 
