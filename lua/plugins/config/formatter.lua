5b294a706bd91d54b7ca751e907bd1c1b5687f80
kaisic1224 <hiddenshadows1224@gmail.com>
2025-01-15 20:27:14 -0600

    add gofmt support

--- lua/plugins/config/formatter.lua
+++ lua/plugins/config/formatter.lua
@@ -4,7 +4,7 @@ conform.setup({
         formatters_by_ft = {
                 python = { "ruff" },
                 javascript = { "prettierd", "prettier", stop_after_first = true },
-                -- go = { }
+                go = { "gofmt" },
         },
         log_level = vim.log.levels.ERROR,
         notify_on_error = true,
